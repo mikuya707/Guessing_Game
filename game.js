28c2de0d39c66633cc716224cd81754e90edaa5d
Karen Mao <karen.mao707@gmail.com>
2015-07-04 11:46:50 -0500

    added basic logic in game.js

--- game.js
+++ game.js
@@ -1,5 +1,27 @@
 $(document).ready(function(){
-    $("p").click(function(){
-        $(this).hide();
+	  var number = Math.floor(Math.random() * 100 + 1);
+	  $('#reset').click(function() {
+    location.reload();
+});
+	 $('#submit').on('click', function(){
+    	var cold = "<p>Too cold!</p>"
+    	var hot = "<p>It's getting hot!</p>"
+    	var win = "<p>You win!</p>"
+        var value = $(".form-group").find('input[name="guess"]').val();
+        if(value < number)
+        {
+        	$('#guesses').append(cold);
+        }
+        else if (value > number)
+        {
+        	$('#guesses').append(hot);
+        }
+        else
+        {
+        	$('#guesses').append(win);
+        }
+        //var test = $('<p>'+number+'</p>');
+        //$('#guesses').append(test);
     });
+  
 });
