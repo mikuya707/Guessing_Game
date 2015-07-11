10947405a7fadf1d28bc2525fcd1a046bde63984
Karen Mao <karen.mao707@gmail.com>
2015-07-11 11:13:31 -0400

    prioritized times running out as the first exception to catch

--- game.js
+++ game.js
@@ -46,68 +46,73 @@ $(document).ready(function(){
         
 
          //when user submit an invalid guess
-    if (value < 1 || value > 100 || isNaN(value)) {
-        $('#msg').text(invalid).css("color", "red");
-        alert("type a valid number between 1 and 100!");
-    }
-    else if (jQuery.inArray(value, guesses) !== -1){
-        $('#msg').text(repeat).css("color", "red");
-    }
-    else{
-        //when user exhausted allowed attempts.
+
         if (times == 0){
             $('#msg').text(tryAgain).css("color", "red");
             alert('You ran out of luck, loser :p');
         }
-        else if(times < 0){
-            $('#msg').text('VALAR MORGHTULIS').css("color", "red");
-        }
         else{
-            guesses.push(value);
-            //when user guess the right answer
-            if (value == number){
-                $('#msg').text(win).css("color", "green");
+            if (value < 1 || value > 100 || isNaN(value)) {
+                $('#msg').text(invalid).css("color", "red");
+                alert("type a valid number between 1 and 100!");
+            }
+            else if (jQuery.inArray(value, guesses) !== -1){
+                $('#msg').text(repeat).css("color", "red");
             }
             else{
-                //find out how far user's guess to the answer.
-                var diff = Math.abs(value-number);
+                //when user exhausted allowed attempts.
+                 if (times == 0){
+                    $('#msg').text(tryAgain).css("color", "red");
+                    alert('You ran out of luck, loser :p');
+                    }
+                else if(times < 0){
+                    $('#msg').text('VALAR MORGHTULIS').css("color", "red");
+                    }
+                else{
+                    //add guessed value into array
+                    guesses.push(value);
+                    //when user guess the right answer
+                    if (value == number){
+                        $('#msg').text(win).css("color", "green");
+                    }
+                    else{
+                        //find out how far user's guess to the answer.
+                        var diff = Math.abs(value-number);
 
-                //Based on diff's range, assign appropriate response.
-                if(5 >= diff){
-                    $('#msg').text(hot).css("color", "red");
-                }
-                else if(10 >= diff){
-                    $('#msg').text(warm).css("color", "orange");
-                }
-                else if (40 >= diff)
-                {
-                    $('#msg').text(cold).css("color", "blue");
-                }
-                else
-                {
-                    $('#msg').text(icecold).css("color", "#00FFFF");
+                        //Based on diff's range, assign appropriate response.
+                        if(5 >= diff){
+                            $('#msg').text(hot).css("color", "red");
+                        }
+                        else if(10 >= diff){
+                             $('#msg').text(warm).css("color", "orange");
+                        }
+                         else if (40 >= diff){                           
+                            $('#msg').text(cold).css("color", "blue");
+                        }
+                        else
+                        {
+                           $('#msg').text(icecold).css("color", "#00FFFF");
+                        }
+                    }
+                    //Suggest user how to adjust their guess
+                    if(value < number){
+                        $("#msg").append("<p style='color:red'>Guess higher.</h1>");
+                    }
+                     else if (value > number){
+                        $("#msg").append("<p style='color:blue'>Guess lower.</h1>");
+                    }
+                    //decrement attempt times
+                    times--;
                 }
             }
-           //Suggest user how to adjust their guess
-           if(value < number){
-                $("#msg").append("<p style='color:red'>Guess higher.</h1>");
-            }
-            else if (value > number){
-                $("#msg").append("<p style='color:blue'>Guess lower.</h1>");
-            }
-
-
-            //decrement attempt times
-            times--;
         }
-    }
         //display user's input in a table.
         $.each(guesses, function( i, val ) {
          $( "#" + i ).text(val);
         });
         
         //update times
-        $('#attempt').html('<p id="attempt">Attempt:  ' + times + '  times</p>');
+        $('#attempt').html('<p id="attempt">Attempt:  ' + times + '  times left.</p>');
 
     });
   
